38e13a41a978a3c3718a624ca262b82b54f8ef54
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-11-27 13:30:54 +0100

    (feat): thank you page and download assests in sync **api and backend created

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod'
-import { privateProcedure, router } from './trpc'
+import { privateProcedure, publicProcedure, router } from './trpc'
 import { TRPCError } from '@trpc/server'
 import { getPayloadClient } from '../get-payload'
 import { stripe } from '../lib/stripe'
@@ -57,7 +57,7 @@ export const paymentRouter = router({
 
       try {
         const stripeSession = await stripe.checkout.sessions.create({
-          success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderIs=${order.id}`,
+          success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
           cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
           payment_method_types: ['card', 'paypal'],
           mode: 'payment',
@@ -74,5 +74,30 @@ export const paymentRouter = router({
 
         return { url: null }
       }
+    }),
+
+  pollOrderStatus: privateProcedure
+    .input(z.object({ orderId: z.string() }))
+    .query(async ({ input }) => {
+      const { orderId } = input
+
+      const payload = await getPayloadClient()
+
+      const { docs: orders } = await payload.find({
+        collection: 'orders',
+        where: {
+          id: {
+            equals: orderId,
+          },
+        },
+      })
+
+      if (!orders.length) {
+        throw new TRPCError({ code: 'NOT_FOUND' })
+      }
+
+      const [order] = orders
+
+      return { isPaid: order._isPaid }
     }),
 })
