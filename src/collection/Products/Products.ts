f1279ceae76bbdff23833d229f075a4e11da97ec
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-11-28 13:05:42 +0100

    final changes

--- src/collection/Products/Products.ts
+++ src/collection/Products/Products.ts
@@ -13,13 +13,78 @@ const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
   return { ...data, user: user.id }
 }
 
+const syncUser: AfterChangeHook<Product> = async ({ req, doc }) => {
+  const fullUser = await req.payload.findByID({
+    collection: 'users',
+    id: req.user.id,
+  })
+
+  if (fullUser && typeof fullUser === 'object') {
+    const { products } = fullUser
+
+    const allIDs = [
+      ...(products?.map((product) =>
+        typeof product === 'object' ? product.id : product
+      ) || []),
+    ]
+
+    const createdProductIDs = allIDs.filter(
+      (id, index) => allIDs.indexOf(id) === index
+    )
+
+    const dataToUpdate = [...createdProductIDs, doc.id]
+
+    await req.payload.update({
+      collection: 'users',
+      id: fullUser.id,
+      data: {
+        products: dataToUpdate,
+      },
+    })
+  }
+}
+
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined
+
+    if (!user) return false
+    if (user.role === 'admin') return true
+
+    const userProductIDs = (user.products || []).reduce<Array<string>>(
+      (acc, product) => {
+        if (!product) return acc
+        if (typeof product === 'string') {
+          acc.push(product)
+        } else {
+          acc.push(product.id)
+        }
+
+        return acc
+      },
+      []
+    )
+
+    return {
+      id: {
+        in: userProductIDs,
+      },
+    }
+  }
+
 export const Products: CollectionConfig = {
   slug: 'products',
   admin: {
     useAsTitle: 'name',
   },
-  access: {},
+  access: {
+    read: isAdminOrHasAccess(),
+    update: isAdminOrHasAccess(),
+    delete: isAdminOrHasAccess(),
+  },
   hooks: {
+    afterChange: [syncUser],
     beforeChange: [
       addUser,
       async (args) => {
