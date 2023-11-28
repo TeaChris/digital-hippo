9254828f5b9b1637824038e0fa863307b3f7586f
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-11-28 10:22:51 +0100

    preparing from production

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,11 @@ import { nextApp, nextHandler } from './next-utils'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { appRouter } from './trpc/index'
 import { inferAsyncReturnType } from '@trpc/server'
+import bodyParser from 'body-parser'
+import { IncomingMessage } from 'http'
+import { stripeWebhookHandler } from './webhooks'
+import nextBuild from 'next/dist/build'
+import path from 'path'
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -18,7 +23,17 @@ const createContext = ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
+export type webhookRequest = IncomingMessage & { rawBody: Buffer }
+
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: webhookRequest, _, buffer) => {
+      req.rawBody = buffer
+    },
+  })
+
+  app.post('/api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler)
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
@@ -27,6 +42,19 @@ const start = async () => {
       },
     },
   })
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info('Next.js is building for production')
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '../'))
+
+      process.exit()
+    })
+
+    return
+  }
 
   app.use(
     '/api/trpc',
